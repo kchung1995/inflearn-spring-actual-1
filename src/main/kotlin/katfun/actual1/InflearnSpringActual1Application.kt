5ce72529aa34ff10402bb2ffcb4379d2f0677684
Kuyho Chung <kchung1995@gmail.com>
2023-08-20 14:10:56 +0900

    Initial commit

---
+++ src/main/kotlin/katfun/actual1/InflearnSpringActual1Application.kt
@@ -0,0 +1,11 @@
+package katfun.actual1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnSpringActual1Application
+
+fun main(args: Array<String>) {
+    runApplication<InflearnSpringActual1Application>(*args)
+}
---
+++ src/test/kotlin/katfun/actual1/InflearnSpringActual1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package katfun.actual1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InflearnSpringActual1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
